ae304be102e22133b68f2b211ebff35c1d9aee95
Brian <bralcasaslo@ittepic.edu.mx>
2018-09-11 20:49:25 -0600

    Sumatoria de listas de forma declarativa

---
+++ facI.py
@@ -0,0 +1,10 @@
+n = 4
+
+factorial = 1
+i =n
+
+while i >= 0:
+    factorial *= i
+    i -= 1
+
+print(factorial)
--- factorial-n.py
+++ factorial-n.py
@@ -1,4 +1,5 @@
 def factorial(n):
     return 1 if n == 0 else n*factorial(n-1)
+    assert type(n) == int and n >= 0
 
 print(factorial(int(input("Calcular factorial de: "))))
